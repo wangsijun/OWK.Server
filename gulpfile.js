cc5e4950adf399535d8ab153ae686ceec5e40ba3
王思俊 <wangsijun1986@hotmail.com>
2016-03-01 01:54:03 +0800

    Added files.

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+/**
+ * Created by wangsijun on 2016/3/9.
+ */
+    //导入工具包 require('node_modules里对应的模块')
+var gulp = require('gulp'),                     //本地安装gulp所用到的地方
+    less = require('gulp-less'),
+    minifyCSS = require('gulp-minify-css');
+
+//定义个testLess任务（自定义任务名称）
+gulp.task('tastLess',function(){
+   gulp.src('public/stylesheets/*.less')    //该任务针对的文件
+   .pipe(less())                                    //该任务调用的模块
+   .pipe(minifyCSS())                           //生成压缩CSS文件
+   .pipe(gulp.dest('src/css'));                //将会在src/css下生成所有的css文件
+});
+
+gulp.task('default',['tastLess']);   //定义默认任务
+
+//gulp.task(name[, deps], fn) 定义任务  name：任务名称 deps：依赖任务名称 fn：回调函数
+//gulp.src(globs[, options]) 执行任务处理的文件  globs：处理的文件路径(字符串或者字符串数组)
+//gulp.dest(path[, options]) 处理完后文件生成路径
