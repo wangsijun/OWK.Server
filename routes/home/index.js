4f57fe2d1fe207aa3f83522bca85f89909adf7d8
王思俊 <wangsijun1986@hotmail.com>
2016-03-31 00:50:45 +0800

    added say hello

--- routes/home/index.js
+++ routes/home/index.js
@@ -1,14 +1,20 @@
 var express = require('express');
+var content = require('../../resource/home/index.source.js');
 var taskRoutes = express.Router();
 
 function index(req,res){
-  res.render('./home/index',{title:'Express hello word2'})
+    var values={
+        module:{
+            sayhello:'你好啊'
+        }
+    };
+    res.render('./home/index',{key:content,values:values});
+
 }
 
-
 function attachRoutes(app) {
-  taskRoutes['GET'.toLowerCase()]('',index);
+    taskRoutes['GET'.toLowerCase()]('',index);
 
-  app.use('/', taskRoutes);
+    app.use('/', taskRoutes);
 }
 module.exports = attachRoutes;
