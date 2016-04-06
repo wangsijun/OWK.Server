97955e783aa010374b9ae26a2e9dfdda5f8c5b08
王思俊 <wangsijun1986@hotmail.com>
2016-04-07 00:44:11 +0800

    Added require-dir module

--- routes/home/index.js
+++ routes/home/index.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var content = require('../../resource/home/index.source.js');
+var user = require('../../db_sql/text/user.js');
 var taskRoutes = express.Router();
 
 function index(req,res){
-    var values={
-        module:{
-            sayhello:'你好啊'
-        }
-    };
-    res.render('./home/index',{key:content,values:values});
-
+    var u = new user();
+    u.getUserDetails(1,function(err,data){
+        var values={
+            module:{
+                sayhello:'你好啊'+data[0].UserId
+            }
+        };
+        res.render('./home/index',{key:content,values:values});
+    });
 }
 
 function attachRoutes(app) {
