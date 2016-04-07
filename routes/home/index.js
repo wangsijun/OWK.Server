78b9365dbbe86babc8bfc27f1e8a54df36a3f822
王思俊 <wangsijun1986@hotmail.com>
2016-04-08 00:05:34 +0800

    Added mssql

--- routes/home/index.js
+++ routes/home/index.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var content = require('../../resource/home/index.source.js');
-var user = require('../../db_sql/text/user.js');
+var user1 = require('../../db_sql/text/user-ms.js');
 var taskRoutes = express.Router();
 
 function index(req,res){
-    var u = new user();
+    var u = new user1();
     u.getUserDetails(1,function(err,data){
         var values={
             module:{
-                sayhello:'你好啊'+data[0].UserId
+                sayhello:'你好啊'+data[0].FirstName+ data[0].LastName
             }
         };
         res.render('./home/index',{key:content,values:values});
